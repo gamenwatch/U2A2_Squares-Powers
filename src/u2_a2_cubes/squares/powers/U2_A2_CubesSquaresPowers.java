1c9f21aa17921e6652b25e5d7f3e27c9907aad53
jalac9997 <jalac9997@7KYM6Y1.Student.UGDSB.ED>
2018-10-24 09:34:38 -0400

    -finished program -improved white space

--- src/u2_a2_cubes/squares/powers/U2_A2_CubesSquaresPowers.java
+++ src/u2_a2_cubes/squares/powers/U2_A2_CubesSquaresPowers.java
@@ -26,7 +26,7 @@ public class U2_A2_CubesSquaresPowers {
         // Declare Variables
         int chosenOption = 0;//what will you do
         double chosenVariable;//the number that the user will efect
-        double chosenExponant;//used in option 3
+        double chosenExponent;//used in option 3
         double outputVariable;//the result
         
         while (chosenOption != 4)
@@ -36,10 +36,10 @@ public class U2_A2_CubesSquaresPowers {
             System.out.println("1... Please select one of the given options...");
             System.out.println("2... Find the value of a number squared...");
             System.out.println("3... Find the value of a number cubed...");
-            System.out.println("4... Exit...");
+            System.out.println("4... Exit...\n");
             
             //Input Option
-            System.out.println("What do you want to do?");
+            System.out.println("What do you want to do?\n");
             chosenOption = keyedInput.nextInt();
 
             //Select Option
@@ -47,16 +47,48 @@ public class U2_A2_CubesSquaresPowers {
                 case 1:
                     System.out.println("What number would you like square?");
                     chosenVariable = keyedInput.nextDouble();
+                    outputVariable = chosenVariable;//set output to chosen
                     
+                    for (int i = 1; i < 2; i = i + 1)
+                    {
+                        outputVariable = outputVariable * chosenVariable;
+                        System.out.println(outputVariable);
+                    }
+                    System.out.println("\n");
                     break;
+                
                 case 2:
-                    System.out.println("2");
+                    System.out.println("What number would you like square?");
+                    chosenVariable = keyedInput.nextDouble();
+                    outputVariable = chosenVariable;//set output to chosen
+                    
+                    for (int i = 1; i < 3; i = i + 1)
+                    {
+                        outputVariable = outputVariable * chosenVariable;
+                        System.out.println(outputVariable);
+                    }
+                    System.out.println("\n");
                     break;
+                
                 case 3:
-                    System.out.println("3");
+                    System.out.println("What number would you like square?");
+                    chosenVariable = keyedInput.nextDouble();
+                    outputVariable = chosenVariable;//set output to chosen
+                    
+                    System.out.println("This number should be to the power of what?");
+                    chosenExponent = keyedInput.nextDouble();
+                    
+                    for (int i = 1; i < chosenExponent; i = i + 1)
+                    {
+                        outputVariable = outputVariable * chosenVariable;
+                        System.out.println(outputVariable);
+                    }
+                    System.out.println("\n");
                     break;
+                
                 case 4:
-                    System.out.println("4");
+                    System.out.println("ALL DONE, CLOSING DOWN!");
+                    System.out.println("\n");
                     break;
             }
             
